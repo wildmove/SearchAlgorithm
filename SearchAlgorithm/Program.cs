6640501fa5aef3cfa249417af7393d46aca597f2
wildmove <muhammad.wildan,ft21@mail.umy.ac.id>
2022-10-28 16:52:20 +0700

    Membuat method LinearSearch

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -73,5 +73,34 @@ namespace SearchAlgorithm
 
             } while ((ch == 'y') || (ch == 'Y'));
         }
+        public void LinearSearch()
+        {
+            char ch;
+            //search for number of comparison
+            int ctr;
+            do
+            {
+                //Accept the number to be searched
+                Console.Write("\n Enter element you want to search \n");
+                int item = Convert.ToInt32(Console.ReadLine());
+
+                ctr = 0;
+                for (i = 0; i < n; i++)
+                {
+                    ctr++;
+                    if (arr[i] == item)
+                    {
+                        Console.WriteLine("\n" + item.ToString() + "found at position" + (i + 1).ToString());
+                        break;
+
+                    }
+                }
+                if (i == n)
+                    Console.WriteLine("\n" + item.ToString() + "found in the array");
+                Console.WriteLine("\nNumber of comparison : " + ctr);
+                Console.Write("\nContinue search (y/n): ");
+                ch = char.Parse(Console.ReadLine());
+            } while ((ch == 'y') || (ch == 'Y'));
+        }
     }
 }
