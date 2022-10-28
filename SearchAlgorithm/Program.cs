3531de75895a18569b188f752ef3d119fe2587fb
wildmove <muhammad.wildan,ft21@mail.umy.ac.id>
2022-10-28 16:02:43 +0700

    Deklarasi variable arr, n, dan i

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -4,6 +4,13 @@ namespace SearchAlgorithm
 {
     class Program
     {
+        //Array to be searched
+        int[] arr = new int[20];
+        //Number of elements in the array
+        int n;
+        //get the number of elements to store in the array
+        int i;
+
         
     }
 }
