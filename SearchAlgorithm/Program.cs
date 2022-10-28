8e83ad64b9a30c96995e7eb5e7738af389eb9b03
wildmove <muhammad.wildan,ft21@mail.umy.ac.id>
2022-10-28 16:15:28 +0700

    Membuat method input

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -11,6 +11,29 @@ namespace SearchAlgorithm
         //get the number of elements to store in the array
         int i;
 
-        
+        public void input()
+        {
+            while(true)
+            {
+                Console.Write("Enter teh number of elements in the array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine("\n Array should have minimum 1 and maximum 20 elements \n");
+            }
+            //Accept array elements 
+            Console.WriteLine("");
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Enter array elements: ");
+            Console.WriteLine("----------------------");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
+        }
     }
 }
